9756d90536345ae7e05c56e2b21d423c977f93a0
Alexpril <alexpril2002@gmail.com>
2023-12-02 14:00:09 +0000

    init-commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace FizzBuzz1
+{
+    public class FizzBuzz
+    {
+        public string CheckResult(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
